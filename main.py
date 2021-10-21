44f612c28995bf0532082360b9c443f43cc88d05
John Peddie <john.peddie@ecs.co.uk>
2021-10-21 10:31:25 +0100

    reformat and added functionality of main.py

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 import requests
 import config
+import pandas as pd
+
+
+def buildAPIConnection():
+    auth = requests.auth.HTTPBasicAuth(config.redditAPIClientID, config.redditAPIClientSecret)
+    data = {'grant_type': 'password',
+            'username': config.redditUsername,
+            'password': config.redditPassword}
+    headers = {'User-Agent': 'PythonPostPuller/0.0.1'}
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers=headers)
+    TOKEN = res.json()['access_token']
+    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+    return headers
+
+
+def fetchSubreddit(subreddit, headers):
+    res = requests.get("https://oauth.reddit.com/r/" + subreddit + "/hot",
+                       headers=headers)
+    return res
+
+def buildDataFrame(res):
+    df = pd.DataFrame()
+    for post in res.json()['data']['children']:
+        df = df.append({
+            'subreddit': post['data']['subreddit'],
+            'title': post['data']['title'],
+            'selftext': post['data']['selftext']
+        }, ignore_index=True)
+    return df
 
 
 
+def main():
+    dfPython = buildDataFrame(fetchSubreddit("python", buildAPIConnection()))
+    print(dfPython)
 
-# note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
-auth = requests.auth.HTTPBasicAuth(config.redditAPIClientID, config.redditAPIClientSecret)
 
-# here we pass our login method (password), username, and password
-data = {'grant_type': 'password',
-        'username': config.redditUsername,
-        'password': config.redditPassword}
-
-# setup our header info, which gives reddit a brief description of our app
-headers = {'User-Agent': 'PythonPostPuller/0.0.1'}
-
-# send our request for an OAuth token
-res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=auth, data=data, headers=headers)
-
-# convert response to JSON and pull access_token value
-TOKEN = res.json()['access_token']
-
-# add authorization to our headers dictionary
-headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
-
-# while the token is valid (~2 hours) we just add headers=headers to our requests
-requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
-
-res = requests.get("https://oauth.reddit.com/r/python/hot",
-                   headers=headers)
-
-print(res.json())
+if __name__ == "__main__":
+    main()

c1b469ba81d11b02ad8a2028d888ca93f11d0839
John Peddie <john.peddie@ecs.co.uk>
2021-12-08 10:27:39 +0000

    added a way to fetch banned subreddit posts, particularly useful for auth training data

---
+++ config.py
@@ -0,0 +1,6 @@
+redditUsername = "GlobalLogicECSUser"
+redditPassword = "ECS!Gl0ballogic"
+redditAPIClientID = "stCMEEf7J7gx6tUKpD9M6g"
+redditAPIClientSecret = "7XypjG42C3HvfTxZmZp46Dl_vt-W7g"
+
+githubPersonalAccessToken = "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"
